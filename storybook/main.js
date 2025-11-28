a79853f1d191d6f90cd757c129fbf49dc5f1ad12
Carolina de Aguiar <carollinadeaguiar@gmail.com>
2025-11-28 16:53:14 +0000

    Create storybook main.js

---
+++ storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // Path to your story files
+  stories: ['../src/**/*.stories.js'],
+
+  // Default set of useful addons
+  addons: [
+    '@storybook/addon-essentials',
+  ],
+
+  // Set the framework you are using (React in this case)
+  framework: '@storybook/react',
+};
